580f368893a63fed6fa3df1b5e9e470cfff932fd
SausageCode <matej.blagsic@protonmail.com>
2018-03-15 16:48:11 +0100

    popravek 1. laboratorijske vaje

--- PROGRAMIRANJE_II/Laboratorijske vaje/1. Laboratorijska vaja/1_lab-vaja.c
+++ PROGRAMIRANJE_II/Laboratorijske vaje/1. Laboratorijska vaja/1_lab-vaja.c
@@ -1,50 +1,40 @@
 #include <stdio.h>
-/*Napiši program, ki sešteje, odšteje, zmnoži ali delji dve kompleksni števili oblike z = a+bi in w = c+di
+/*Napiši program, ki sešteje, odšteje, zmnoži ali delji dve kompleksni števili oblike z = a+bi in w = c+di, ki sta podana s strani uporabnika v obliki z = a+bi
 */
 
 int main(){
 
-	int a=4;
-	int b=-5;
-	int c=-5;
-	int d=1;
+	int a=0;
+	int b=0;
+	int c=0;
+	int d=0;
 	char operator;
 
+	printf("Vnesi prvo kompleksno stevilo v obliki a+bi	");
+	scanf("\n%d %d", &a, &b);
+	printf("Vnesi drugo kompleksno stevilo v obliki a+bi	");
+	scanf("\n%d %d", &c, &d);
 	printf("Stevili sta z = %d%+di ter w = %d%+di.\n", a, b, c, d);
-
-	printf("Vsota stevil z in w je %d%+di \n", a+c, b+d);
-	printf("Razlika stevil z in w je %d%+di\n", a-c, b-d);
-	printf("Produkt stevil z in w je %d%+di \n", a*c-b*d, a*d+b*c);
-	printf("Kvocient stevil z in w je %.2f%+.2fi \n", (float)(a*c+b*d)/(c*c+d*d),(float)(b*c-a*d)/(c*c+d*d));
-
-/*	Vnašanje in ibira aritmetične operacije iz strani uporabnika
-	printf("Vnesi realno komponento kompleksnega števila z\n");
-	scanf(" %d", &a);
-	printf("Vnesi imaginarno komponento kompleksnega števila z\n");
-	scanf(" %d", &b);
-	printf("Vnesi realno komponento kompleksnega števila w\n");
-	scanf(" %d", &c);
-	printf("Vnesi imaginarno komponento kompleksnega števila w\n");
-	scanf(" %d", &d);
-	printf("Vnesi aritmetični operator:\n");
-	scanf(" %c",&operator);
+	printf("Vnesi aritmeticni operator:	");
+	scanf("\n %c",&operator);
 
 	switch(operator){
 		case '+':
-			printf("Vsota števil z in w je %d%+di \n", a+c, b+d);
+			printf("Vsota stevil z in w je %d%+di \n", a+c, b+d);
 			break;
 		case '-':
-			printf("Razlika števil z in w je %d%+di\n", a-c, b-d);
+			printf("Razlika stevil z in w je %d%+di\n", a-c, b-d);
 			break;
 		case '*':
-			printf("Produkt števil z in w je %d%+di\n", a*c-b*d, a*d+b*c);
+			printf("Produkt stevil z in w je %d%+di\n", a*c-b*d, a*d+b*c);
 			break;
 		case '/':
+			if((c*c+d*d) == 0){printf("Deljenje ni dovoljeno! Ponovno pozenem program.\n");main();}
 			printf("Kvocient stevil z in w je %.2f%+.2fi \n", (float)(a*c+b*d)/(c*c+d*d),(float)(b*c-a*d)/(c*c+d*d));
 			break;
 		default:
 			printf("Neveljaven vnos!");
 			main();
-	}*/
+	}
 	return 0;
 }
